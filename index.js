20d6d78bac5001d40747b885d9c6ceb9522b9390
robert boudreaux <boudreaux.r@gmail.com>
2023-08-21 23:26:47 +0200

    mofifyingdom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+var newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Rob is the champion";
